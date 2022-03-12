cedabd25e8b6b3e8546432c8ab0cb359e1899505
taniryla <ryland0000@gmail.com>
2022-03-11 23:21:47 -0700

    fixing type in category models

--- models/itemSchema.js
+++ models/itemSchema.js
@@ -5,8 +5,7 @@ const Schema = require('mongoose').Schema;
 const itemSchema = new Schema({
   name: { type: String, required: true },
   category: {
-    type: String,
-    enum: ['keto', 'paleo', 'pescatarian', 'vegetarian', 'other'],   
+    type: Schema.Types.ObjectId,
     ref: 'Category'
   },
   calories: { type: Number, required: true },
