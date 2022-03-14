1af80eb01aa714156e1fac017cf8bf4953be527a
taniryla <ryland0000@gmail.com>
2022-03-13 21:01:45 -0600

    fixed the foodlist add btn size

--- src/components/FoodListItem/FoodListItem.jsx
+++ src/components/FoodListItem/FoodListItem.jsx
@@ -10,12 +10,12 @@ export default function FoodListItem({ food }) {
       <>
         <div className="card">
          <div className='FoodListItem grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
-            <img className="card-img-top img-fluid img-thumbnail rounded float-left" src={food.image} alt="Food Image" /> 
+            <img className="card-img-top img-fluid img-thumbnail rounded float-right" src={food.image} alt="Food Image" /> 
             <div className="addtofoodlog card=body">
-                  <div className="card-title">{food.name}</div>
-                  <span className="card-text">{food.calories} cal</span> 
+                  <div className="card-title text-right">{food.name}</div>
+                  <div className="card-text text-right">{food.calories} cal</div> 
                   <br></br>
-                   <button className="btn btn-sm btn-block" onClick={() => handleAddToFoodLog(food._id)}>
+                   <button className="btn btn-sm btn-outline float-right" onClick={() => handleAddToFoodLog(food._id)}>
                     ADD
                     </button> 
             </div>
