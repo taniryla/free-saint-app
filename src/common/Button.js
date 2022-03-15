cf117643d1bd86220d1e4517615959838abe35d6
taniryla <ryland0000@gmail.com>
2022-03-14 23:26:53 -0600

    added css helper to Button component

--- src/common/Button.js
+++ src/common/Button.js
@@ -1,10 +1,19 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
  const Button = styled.button`
     color: #148980;
     background: ${props => props.secondary ? "#c5c5c6" : "white" } ;
     font-weight: bold;
-    padding: 3vmin;
+    ${props => props.large ? css`
+    padding: 5vmin;
+    border-radius: 3vmin;
+    font-size: 1.5em;
+    `  : css`
+    padding: 2.5vmin;
+    border-radius: 1.5vmin;
+    font-size: 0.75em;
+    `}
+
     border-radius: 1vmin;
     font-size: 1em;
     border: none;
